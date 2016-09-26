79da0ad4dccb3fdc6fd9cc24736c370bc72564dd
4000D <dkdkajej@gmail.com>
2016-09-26 20:15:27 +0900

    test

--- src/react/components/Header.js
+++ src/react/components/Header.js
@@ -1,12 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 
+let style = {
+	display: 'flex',
+	width: '100%',
+};
+
 class Header extends Component {
+
 
 	render() {
 		let nav = this.props.children;
 
 		return (
-			<header>
+			<header style={style}>
 				<div>
 					<img src="" alt="our image"/>
 				</div>
--- src/react/components/Nav.js
+++ src/react/components/Nav.js
@@ -1,16 +1,24 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
+let ulStyle = {
+	display: 'inline-block'
+};
 
+let liStyle = {
+	display: 'inline-block'
+};
 
 class Nav extends Component {
+
+
 	render() {
 		return (
 			<ul>
-				<li><Link to='/home' activeClassName='active'>Home</Link></li>
-				<li><Link to='/projects' activeClassName='active'>Projects</Link></li>
-				<li><Link to='/members' activeClassName='active'>Members</Link></li>
-				<li><Link to='/about' activeClassName='active'>About</Link></li>
+				<li ><Link to='/' activeClassName='active'>Home</Link></li>
+				<li ><Link to='/projects' activeClassName='active'>Projects</Link></li>
+				<li ><Link to='/members' activeClassName='active'>Members</Link></li>
+				<li ><Link to='/about' activeClassName='active'>About</Link></li>
 			</ul>
 		)
 	}
